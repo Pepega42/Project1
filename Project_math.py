36b537579accbaca6f0f76c4ba012063380f1a37
PepegaSlammer <dimadima.shin@mail.ru>
2023-09-17 23:36:59 +0300

    project1

---
+++ Project_math.py
@@ -0,0 +1,73 @@
+import math
+while True:
+    calc = input("+, -, /, **, sqrt, !, sin, cos, tg: ")    
+    if calc == "+":
+        try:
+            numb1 = float(input("Введите первое число: "))
+            numb2 = float(input("Введите второе число: "))
+            print(numb1 + numb2)
+        except:
+            print("Данное число не сработает")
+    elif calc == "-":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb2 = float(input("Введите второе число: "))
+            print(numb1 - numb2)
+        except:
+            print("Данное число не сработает")
+    elif calc == "*":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb2 = float(input("Введите второе число: "))
+            print(numb1 * numb2)
+        except:
+            print("Данное число не сработает")
+    elif calc == "/":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb2 = float(input("Введите второе число: "))
+            print(numb1 / numb2)
+        except:
+            print("Данное число не сработает")
+    elif calc == "**":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb2 = float(input("Введите второе число: "))
+            print(numb1 ** numb2)
+        except:
+            print("Данное число не сработает")
+    elif calc == "**":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            print(math.sqrt(numb1))
+        except:
+            print("Данное число не сработает")
+    elif calc == "!":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            print(math.factorial(numb1))
+        except:
+            print("Данное число не сработает")
+    elif calc == "sin":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb1 = math.radians(numb1)
+            print(math.sin(numb1))
+        except:
+            print("Данное число не сработает")
+    elif calc == "cos":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb1 = math.radians(numb1)
+            print(math.cos(numb1))
+        except:
+            print("Данное число не сработает")
+    elif calc == "tg":
+        try:    
+            numb1 = float(input("Введите первое число: "))
+            numb1 = math.radians(numb1)
+            print(math.tan(numb1))
+        except:
+            print("Данное число не сработает")
+    else:
+        print("Такой операции нет")
